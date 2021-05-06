00d077b3f65189ee03eba674b3a6b533ac435e38
tiandongbo <tiandongbo@aliyun.com>
2021-05-06 11:28:37 +0800

    add DBHelper

---
+++ DBHelper/src/main/java/com/huawei/mes/util/DBHelper.java
@@ -0,0 +1,138 @@
+package com.huawei.mes.util;
+
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
+
+/**
+ * ClassName GaussHelper
+ * Description
+ * Create by tiandongbo 00350982
+ * Date 2021/4/30 20:59
+ */
+public class DBHelper {
+    //定义连接资源
+    private static Connection ct = null;
+    private static PreparedStatement ps = null;
+    private static CallableStatement cs = null;
+    private static ResultSet rs = null;
+
+    //定义配置参数
+    private static String url = null;
+    private static String username = null;
+    private static String password = null;
+    private static String driver = "com.huawei.gauss.jdbc.inner.GaussDriver";
+
+    private static Properties pp = null;
+    private static InputStream is = null;
+
+    //读取配置参数，加载驱动
+    static {
+        try {
+            pp = new Properties();
+            is = DBHelper.class.getClassLoader().getResourceAsStream("jdbc.properties");
+            if (is == null) {
+                is = ClassLoader.getSystemResourceAsStream("jdbc.properties");
+            }
+            pp.load(is);
+            driver = pp.getProperty("driver");
+            url = pp.getProperty("url");
+            username = pp.getProperty("username");
+            password = pp.getProperty("password");
+            Class.forName(driver);
+        } catch (Exception e) {
+            e.printStackTrace();
+            //System.exit(0);
+            System.out.println(" 读取数据库连接配置文件jdbc.properties失败!");
+        } finally {
+            try {
+                is.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            is = null;
+        }
+    }
+
+
+    //获取连接
+    public static Connection getConnection() {
+        try {
+            ct = DriverManager.getConnection(url, username, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ct;
+    }
+
+    //关闭所有资源连接
+    public static void close(Connection ct, Statement ps, ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            rs = null;
+        }
+        if (ps != null) {
+            try {
+                ps.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            ps = null;
+        }
+        if (null != ct) {
+            try {
+                ct.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            ct = null;
+        }
+    }
+
+
+    //执行DQL查询
+    public static ResultSet executeQuery(String sql, String[] parameters) {
+        try {
+            ct = getConnection();
+            ps = ct.prepareStatement(sql);
+            if (parameters != null) {
+                for (int i = 0; i < parameters.length; i++) {
+                    ps.setString(i + 1, parameters[i]);
+                }
+            }
+            rs = ps.executeQuery();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e.getMessage());
+        }
+        return rs;
+    }
+
+    //执行DML更新
+    public static int executeUpdate(String sql, String[] parameters) {
+        try {
+            ct = getConnection();
+            ps = ct.prepareStatement(sql);
+            if (parameters != null) {
+                for (int i = 0; i < parameters.length; i++) {
+                    ps.setString(i + 1, parameters[i]);
+                }
+            }
+            return ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e.getMessage());
+        } finally {
+            close(ct, ps, rs);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
